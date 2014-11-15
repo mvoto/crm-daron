b8cdc5cacf1a577366860c2ba6ce2d732f079aaf
Mauricio Voto <mauriciovoto@gmail.com>
2014-11-15 15:57:35 -0200

    Add a nice seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-FactoryGirl.create(:person)
-FactoryGirl.create(:company)
+require File.expand_path("../../spec/support/factory_girl_helper", __FILE__)
+
+include FactoryGirlHelper
+
+State.all.each_with_index do |state, i|
+	address = FactoryGirl.create(:address, state_id: state.id,
+		city_id: state.cities.first)
+
+	person = find_or_create(:person, email: "test#{i}@example.com")
+	person.address = address
+	person.save
+
+	company = find_or_create(:company, email: "test#{i}@example.com")
+	company.address = address
+	company.save
+end
