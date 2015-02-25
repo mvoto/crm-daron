7bd313495d64261733cd3dfd9c578095df0df556
Mauricio Voto <mauriciovoto@gmail.com>
2015-02-25 20:07:39 -0300

    Add ddd phone helpers

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -3,4 +3,11 @@ module CustomersHelper
     customer.address ||= Address.new
     customer
   end
+
+  [:phone, :cellphone].each do |method_name|
+    define_method "complete_#{method_name}" do |customer|
+      ddd = "#{method_name}_ddd"
+      "#{customer.send(ddd)} #{customer.send(method_name)}"
+    end
+  end
 end
