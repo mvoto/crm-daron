c02117b9ca018ec3d123a967a381f563569bf948
Mauricio Voto <mauriciovoto@gmail.com>
2015-06-03 15:43:09 -0300

    comment seeds for prod db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@ State.all.each_with_index do |state, i|
 	address = FactoryGirl.create(:address, state_id: state.id,
 		city_id: state.cities.first.id)
 
-	person = find_or_create(:person, email: "test#{i}@example.com")
-	person.address = address
-	person.save
+	# person = find_or_create(:person, email: "test#{i}@example.com")
+	# person.address = address
+	# person.save
 
-	company = find_or_create(:company, email: "test#{i}@example.com")
-	company.address = address
-	company.save
+	# company = find_or_create(:company, email: "test#{i}@example.com")
+	# company.address = address
+	# company.save
 end
