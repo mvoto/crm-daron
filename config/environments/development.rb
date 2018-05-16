72a72a4de39c97161f4b799fbe1804b21f774396
Mauricio Voto <mauriciovoto@gmail.com>
2018-05-16 01:05:59 -0700

    attempt to fix broken specs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+
+  config.action_mailer.default_url_options = { host: 'localhost' }
 end
