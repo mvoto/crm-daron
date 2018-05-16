1a769ce6b165e8ce1e734ff89cbd0ba1d2e88220
Mauricio Voto <mauriciovoto@gmail.com>
2018-05-16 02:15:14 -0700

    add missing host for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,5 @@ Rails.application.configure do
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD']
   }
+  config.action_mailer.default_url_options = { host: 'daron.herokuapp.com' }
 end
