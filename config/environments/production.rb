de8b1d17710443011947992ed34e3533ae31b294
Mauricio Voto <mauriciovoto@gmail.com>
2018-06-18 13:00:13 -0700

    update email sender host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD']
   }
-  config.action_mailer.default_url_options = { host: 'daron-teste.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'daron.herokuapp.com' }
 end
