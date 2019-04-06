4352c160a317c7aac43b0ecd0d168f2022f953ed
Mauricio Voto <mauriciovoto@gmail.com>
2019-04-06 15:12:00 -0700

    add proper missing domain :/

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'daron.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
