c6b32dd1d4375b882c8bdc4d07565407ae05cb4e
Mauricio Voto <mauriciovoto@gmail.com>
2019-04-06 14:54:23 -0700

    minor chore adjustments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-require File.expand_path('spec/support/factory_girl_helper.rb', __dir__)
+require File.expand_path('../spec/support/factory_girl_helper.rb', __dir__)
 
-FactoryGirl.create(:user, email: 'daron@daron.com.br', password: 'daron2015')
+FactoryGirl.create(:user, email: 'daron@daron.com.br', password: 'daron2015', confirmed_at: DateTime.now, role: User::ADMIN)
 
 State.all.each_with_index do |state, i|
   address = FactoryGirl.create(:address, state_id: state.id, city_id: state.cities.first.id)
 
-  person = find_or_create(:person, email: "test#{i}@example.com")
+  person = FactoryGirlHelper.find_or_create(:person, email: "test#{i}@example.com")
   person.address = address
   person.save
 
-  company = find_or_create(:company, email: "test#{i}@example.com")
+  company = FactoryGirlHelper.find_or_create(:company, email: "test#{i}@example.com")
   company.address = address
   company.save
 end
--- spec/support/factory_girl_helper.rb
+++ spec/support/factory_girl_helper.rb
@@ -1,28 +1,28 @@
 # See more at: http://blog.tobedevoured.com/post/54523066986/rspec-helper-to-add-find-or-create-to-factorygirl#sthash.n79NQWlu.dpuf
-module FactoryGirlHelper
+class FactoryGirlHelper
+  class << self
+    def find_or_create(*args)
+      name = args.shift
+      clazz = nil
 
-  def find_or_create(*args)
-    name = args.shift
-    clazz = nil
+      # convert from underscores String to camelcase Class
+      if name.is_a? Hash
+        name = name.first[0]
+        clazz = name.first[1].to_s.camelize.constantize
+      else
+        clazz = name.to_s.camelize.constantize
+      end
 
-    # convert from underscores String to camelcase Class
-    if name.is_a? Hash
-      name = name.first[0]
-      clazz = name.first[1].to_s.camelize.constantize
-    else
-      clazz = name.to_s.camelize.constantize
+      target = nil
+
+      # Create Arel lookup to see if model already exists
+      lookup = args.shift
+      target = clazz.where(lookup).first unless lookup.empty?
+
+      # IF the model was not found, create a new one
+      target = FactoryGirl.create(name,lookup) if target.nil?
+
+      target
     end
-
-    target = nil
-
-    # Create Arel lookup to see if model already exists
-    lookup = args.shift
-    target = clazz.where(lookup).first unless lookup.empty?
-
-    # IF the model was not found, create a new one
-    target = FactoryGirl.create(name,lookup) if target.nil?
-
-    target
   end
-
 end
