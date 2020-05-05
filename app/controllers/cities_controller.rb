01f9510d6be2ee4339a0d0f7e99e50d5a0164f33
Mauricio Voto <mauriciovoto@gmail.com>
2020-05-05 09:30:26 -0700

    persists city full name after filter submit

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,4 +7,14 @@ class CitiesController < ApplicationController
       format.json { render json: cities }
     end
   end
+
+  def show
+    city = City.find(params[:id])
+    city_as_json = city.as_json
+    city_as_json[:full_name] = city.full_name
+
+    respond_to do |format|
+      format.json { render json: city_as_json }
+    end
+  end
 end
