f506a72eb24129b10b2466715b0a6d4839522234
Mauricio Voto <mauriciovoto@gmail.com>
2020-06-24 08:08:57 -0700

    bringing back gmail instead of mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,22 +84,22 @@ Rails.application.configure do
 	# Do not dump schema after migrations.
 	config.active_record.dump_schema_after_migration = false
 
-	config.action_mailer.smtp_settings = {
-		:port           => ENV['MAILGUN_SMTP_PORT'],
-		:address        => ENV['MAILGUN_SMTP_SERVER'],
-		:user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-		:password       => ENV['MAILGUN_SMTP_PASSWORD'],
-		:domain         => 'daron.herokuapp.com',
-		:authentication => :plain,
-	}
-	config.action_mailer.delivery_method = :smtp
 	# config.action_mailer.smtp_settings = {
-	# 	address: 'smtp.gmail.com',
-	# 	port: 587,
-	# 	authentication: 'plain',
-	# 	enable_starttls_auto: true,
-	# 	user_name: ENV['GMAIL_USERNAME'],
-	# 	password: ENV['GMAIL_PASSWORD']
+	# 	:port           => ENV['MAILGUN_SMTP_PORT'],
+	# 	:address        => ENV['MAILGUN_SMTP_SERVER'],
+	# 	:user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+	# 	:password       => ENV['MAILGUN_SMTP_PASSWORD'],
+	# 	:domain         => 'daron.herokuapp.com',
+	# 	:authentication => :plain,
 	# }
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		address: 'smtp.gmail.com',
+		port: 587,
+		authentication: 'plain',
+		enable_starttls_auto: true,
+		user_name: ENV['GMAIL_USERNAME'],
+		password: ENV['GMAIL_PASSWORD']
+	}
 	config.action_mailer.default_url_options = { host: 'daron.herokuapp.com' }
 end
